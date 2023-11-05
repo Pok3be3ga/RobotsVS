4ed0511e75cca71db9a5bf4411389e99447cc55a
MrSpanker <mrspankerman@gmail.com>
2023-11-05 18:57:22 +0300

    звуки не складываются

--- Assets/Scripts/DelayForSound.cs
+++ Assets/Scripts/DelayForSound.cs
@@ -5,21 +5,22 @@ using UnityEngine;
 public class DelayForSound : MonoBehaviour
 {
     [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private AudioClip _audioClip;
     private bool _isPlaying;
 
     public void PlaySound()
     {
         if (!_isPlaying)
         {
-            StartCoroutine(PlaySoundWithDelay());
-            _isPlaying = false;
+            StartCoroutine(PlaySoundWithDelay());            
         }
     }
 
     private IEnumerator PlaySoundWithDelay()
     {
         _isPlaying = true;
-        _audioSource.Play();
+        _audioSource.PlayOneShot(_audioClip);
         yield return new WaitForSeconds(0.3f);
+        _isPlaying = false;
     }
 }
