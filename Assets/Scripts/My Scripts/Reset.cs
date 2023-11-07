48ca7dd01ccd9b477c66ddb6fa8bded0478b0e57
Pok3be3ga <pok3be3ga2004@gmail.com>
2023-11-07 20:26:48 +0300

    123

--- Assets/Scripts/My Scripts/Reset.cs
+++ Assets/Scripts/My Scripts/Reset.cs
@@ -6,10 +6,12 @@ using YG;
 
 public class Reset : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI _textMeshPro;
+    [SerializeField] TextMeshProUGUI _textMeshPro; 
+    [SerializeField] TextMeshProUGUI _textMeshPro2;
     private void Start()
     {
-        _textMeshPro.text = YandexGame.EnvironmentData.language;
+        _textMeshPro.text = YandexGame.savesData.language.ToString();
+        _textMeshPro2.text = YandexGame.EnvironmentData.language;
     }
     public void ResetProgress()
     {
--- Assets/YandexGame/WorkingData/SavesYG.cs
+++ Assets/YandexGame/WorkingData/SavesYG.cs
@@ -7,7 +7,7 @@ namespace YG
         // "Технические сохранения" для работы плагина (Не удалять)
         public int idSave;
         public bool isFirstSession = true;
-        public string language = "en";
+        public string language = "ru";
         public bool promptDone;
         // Ваши сохранения
 
